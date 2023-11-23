a528a6c812fb1b2fadb1ce5396667f5f291f6e37
Maslov <maslowalexey021@gmail.com>
2023-11-23 22:11:42 +0300

    Selectors

---
+++ src/test/java/any/selectors.java
@@ -0,0 +1,19 @@
+package any;
+
+public class selectors {
+    String url = "https://demoqa.com/automation-practice-form";
+    String firstName ="//input[@id=('firstName')]";
+    String lastName ="//input[@id=('lastName')]";
+    String email = "//input[@id=('userEmail')]";
+    String genderRadio = "//input[@id=('gender-radio-1')]";
+    String dateBirth = "//input[@id=('dateOfBirthInput')]";
+    String subjectContainer = "//input[@id=('subjectsContainer')]";
+    String checkboxReading = "//input[@id=('hobbies-checkbox-2')]";
+    String checkboxSport = "//input[@id=('hobbies-checkbox-1')]";
+    String checkboxMusic = "//input[@id=('hobbies-checkbox-3')]";
+    String adress = "//div/textarea[@id=('currentAddress')]";
+    String state = "//*[@id='state']";
+    String city = "//div[@id='city']";
+    String submit = "//button[@id=('submit')]";
+
+}
