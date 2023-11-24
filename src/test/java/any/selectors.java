0d0ddabb00c8ae93bba4f5b15ae20c7a695f2428
Maslov <Masowalexey021@gmail.vom>
2023-11-24 10:41:21 +0300

    Добавил методы для получения селекторов со страницы demoqa

--- src/test/java/any/selectors.java
+++ src/test/java/any/selectors.java
@@ -1,19 +1,67 @@
 package any;
 
-public class selectors {
-    String url = "https://demoqa.com/automation-practice-form";
-    String firstName ="//input[@id=('firstName')]";
-    String lastName ="//input[@id=('lastName')]";
-    String email = "//input[@id=('userEmail')]";
-    String genderRadio = "//input[@id=('gender-radio-1')]";
-    String dateBirth = "//input[@id=('dateOfBirthInput')]";
-    String subjectContainer = "//input[@id=('subjectsContainer')]";
-    String checkboxReading = "//input[@id=('hobbies-checkbox-2')]";
-    String checkboxSport = "//input[@id=('hobbies-checkbox-1')]";
-    String checkboxMusic = "//input[@id=('hobbies-checkbox-3')]";
-    String adress = "//div/textarea[@id=('currentAddress')]";
-    String state = "//*[@id='state']";
-    String city = "//div[@id='city']";
-    String submit = "//button[@id=('submit')]";
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$x;
+
+public class selectors {
+
+    public SelenideElement url() {
+        return $x("https://demoqa.com/automation-practice-form")
+                .as("Форма для тестирования");
+    }
+    public SelenideElement firstName() {
+        return $x("//input[@id=('firstName')]")
+                .as("Имя");
+    }
+    public SelenideElement lastName() {
+        return $x("//input[@id=('lastName')]")
+                .as("Фамилия");
+    }
+    public SelenideElement email() {
+        return $x("//input[@id=('userEmail')]")
+                .as("Почта");
+    }
+    public SelenideElement genderRadio() {
+        return $x("//input[@id=('gender-radio-1')]")
+                .as("Гендер");
+    }
+    public SelenideElement dateBirth() {
+        return $x("//input[@id=('dateOfBirthInput')]")
+                .as("Дата рождения");
+    }
+    public SelenideElement subjectContainer() {
+        return $x("//input[@id=('subjectsContainer')]")
+                .as("Текстовое поле сабж");
+    }
+
+    public SelenideElement checkboxReading() {
+        return $x("//input[@id=('hobbies-checkbox-2')]")
+                .as("Чекбокс чтение");
+    }
+    public SelenideElement checkboxSport() {
+        return $x("//input[@id=('hobbies-checkbox-1')]")
+                .as("Чекбокс спорт");
+    }
+    public SelenideElement checkboxMusic() {
+        return $x("//input[@id=('hobbies-checkbox-3')]")
+                .as("Чекбокс музыка");
+    }
+    public SelenideElement adress() {
+        return $x("//div/textarea[@id=('currentAddress')]")
+                .as("Адрес");
+    }
+    public SelenideElement state() {
+        return $x("//*[@id='state']")
+                .as("Штат");
+    }
+    public SelenideElement city() {
+        return $x("//div[@id='city']")
+                .as("Город");
+    }
+    public SelenideElement submit() {
+        return $x("//button[@id=('submit')]")
+                .as("Кнопка ");
+    }
 }
