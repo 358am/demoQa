78bd122edfdd39594a1bccbf03424804f4b2f32f
Maslov <maslowalexey021@gmail.com>
2023-11-26 20:47:58 +0300

    Добавил класс steps, в нем шаги для будующих тестов

--- src/test/java/Steps/PracticeForm.java
+++ src/test/java/Steps/PracticeForm.java
@@ -1,7 +1,9 @@
-package Steps;
+package steps;
 
 import any.Selectors;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 /**
@@ -9,11 +11,30 @@ import static com.codeborne.selenide.Selenide.open;
  * **/
 
 public class PracticeForm {
-    private static final Selectors page = new Selectors();
+    private static final Selectors Selectors = new Selectors();
 
 
 
+    // Открыть форму
     public void login() {
-        open("");
+        open(Selectors.url);
     }
+    // Заполнить поле Имя
+    public void setValueName(String name) {
+        $(Selectors.firstName().setValue(name));
+        $(Selectors.firstName().shouldHave(text(name)));
+    }
+    // Заполнить поле Фамилия
+    public void setValueFamily(String family) {
+        $(Selectors.lastName().setValue(family));
+        $(Selectors.firstName().shouldHave(text(family)));
+    }
+    // Заполнить поле Емэйл
+    public void setValueEmail(String email) {
+        $(Selectors.email().setValue(email));
+        $(Selectors.email().shouldHave(text(email)));
+    }
+
+
+
 }
