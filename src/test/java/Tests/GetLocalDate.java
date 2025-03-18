37d75bb69372a7bc2713ed4a523014e7ccd19898
Alexey <maslowalexey021@gmail.com>
2025-03-18 15:18:43 +0300

    Класс для получения даты в формате dd MMMM,yyyy

---
+++ src/test/java/Tests/GetLocalDate.java
@@ -0,0 +1,17 @@
+package Tests;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+public class GetLocalDate {
+
+    String getDate() {
+        LocalDate localDate = LocalDate.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.ENGLISH);
+        String dateNow = localDate.format(dateTimeFormatter);
+        //System.out.println(dateNow);
+        return dateNow;
+    }
+}
