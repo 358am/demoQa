3205c0a953975053c0025571ccf1d2a7b100515a
Alexey <maslowalexey021@gmail.com>
2025-03-27 13:38:36 +0300

    Метод для чтения данных из файла возращает данные в формате строки

---
+++ src/test/java/Tests/ReadTextFromFile.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+// Метод для чтения данных из файла в формате строки
+public class ReadTextFromFile {
+    public static String readTextFromFile(String filePath) {
+        String fileContent = null;
+        try {
+            fileContent = FileUtils.readFileToString(new File(filePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return fileContent;
+    }
+}
